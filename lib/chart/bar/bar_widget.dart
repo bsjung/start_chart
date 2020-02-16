fe4b2148d888ed373830774341d5b05a2b527866
Benjamin Jung <bsjung@gmail.com>
2020-02-17 04:53:05 +0900

    Update start_chart  - seperate directory for bar/line/pie.

---
+++ lib/chart/bar/bar_widget.dart
@@ -0,0 +1,208 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class BarChart extends StatelessWidget {
+  BarChart({
+    @required this.data,
+    @required this.labels,
+    this.color = Colors.blue,
+    this.size = const Size(150, 150),
+  }) : assert(data != null);
+
+  final List<double> data;
+  final List<String> labels;
+  final Color color;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return new LimitedBox(
+      child: new CustomPaint(
+        size: size,
+        painter: new _BarChartPainter(
+           data: data, labels: labels, color : color),
+      ), // CustomPaint
+    ); // LimitedBox
+  }
+}
+
+class _BarChartPainter extends CustomPainter {
+  Color color;
+  double textScaleFactorXAxis = 1.0;
+  double textScaleFactorYAxis = 1.2;
+
+  List<double> data = [];
+  List<String> labels = [];
+  double bottomPadding = 0.0;
+  double leftPadding = 0.0;
+
+  _BarChartPainter({this.data, this.labels, this.color = Colors.blue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    setTextPadding(size);
+
+    List<Offset> coordinates = getCoordinates(size);
+
+    drawBar(canvas, size, coordinates);
+    drawXLabels(canvas, size, coordinates);
+    drawYLabels(canvas, size, coordinates);
+    drawLines(canvas, size, coordinates);
+  }
+
+  void setTextPadding(Size size) {
+    bottomPadding = size.height / 10;
+    leftPadding = size.width / 10;
+  }
+
+  void drawBar(Canvas canvas, Size size, List<Offset> coordinates) {
+    Paint paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+
+    double barWidthMargin = (size.width * 0.09);
+
+    for (var index = 0; index < coordinates.length; index++) {
+      Offset offset = coordinates[index];
+      double left = offset.dx;
+      double right = offset.dx + barWidthMargin;
+      double top = offset.dy;
+      double bottom = size.height - bottomPadding;
+
+      Rect rect = Rect.fromLTRB(right, top, left, bottom);
+      canvas.drawRect(rect, paint);
+    }
+  }
+
+  void drawXLabels(Canvas canvas, Size size, List<Offset> coordinates) {
+    double fontSize = calculateFontSize(labels[0], size, xAxis: true);
+
+    for (int index = 0; index < labels.length; index++) {
+
+      TextSpan span = TextSpan(
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: fontSize,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w400),
+          text: labels[index]);
+
+      TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+      tp.layout();
+
+      Offset offset = coordinates[index];
+      double dx = offset.dx;
+      double dy = size.height - tp.height;
+
+      tp.paint(canvas, Offset(dx, dy));
+    }
+  }
+
+  void drawYLabels(Canvas canvas, Size size, List<Offset> coordinates) {
+    double bottomY = coordinates[0].dy;
+    double topY = coordinates[0].dy;
+    int indexOfMax = 0;
+    int indexOfMin = 0;
+
+    for (int index = 0; index < coordinates.length; index++) {
+      double dy = coordinates[index].dy;
+
+      if (bottomY < dy) {
+        bottomY = dy;
+        indexOfMin = index;
+      }
+
+      if (topY > dy) {
+        topY = dy;
+        indexOfMax = index;
+      }
+    }
+
+    String maxValue = "${data[indexOfMax].toInt()}";
+    String minValue = "${data[indexOfMin].toInt()}";
+
+    double fontSize = calculateFontSize(maxValue, size, xAxis: false);
+
+    drawYText(canvas, maxValue, fontSize, topY);
+    drawYText(canvas, minValue, fontSize, bottomY);
+  }
+
+  double calculateFontSize(String value, Size size, {bool xAxis}) {
+    int numberOfCharacters = value.length;
+    double fontSize = size.width / (numberOfCharacters *  data.length);
+
+    if (xAxis) {
+      fontSize *= textScaleFactorXAxis;
+    } else {
+      fontSize *= textScaleFactorYAxis;
+    }
+
+    return fontSize;
+  }
+
+  void drawLines(Canvas canvas, Size size, List<Offset> coordinates) {
+    Paint paint = Paint()
+      ..color = Colors.blueGrey[100]
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1.8;
+
+    double bottom = size.height - bottomPadding;
+    double left = coordinates[0].dx;
+
+    Path path = Path();
+    path.moveTo(left, 0);
+    path.lineTo(left, bottom);
+    path.lineTo(size.width, bottom);
+
+    canvas.drawPath(path, paint);
+  }
+
+  void drawYText(Canvas canvas, String text, double fontSize, double y) {
+
+    TextSpan span = TextSpan(
+      style: TextStyle(
+          fontSize: fontSize,
+          color: Colors.black,
+          fontFamily: 'Roboto',
+          fontWeight: FontWeight.w600),
+      text: text,
+    );
+
+    TextPainter tp = TextPainter(text: span, textDirection: TextDirection.ltr);
+
+    tp.layout();
+
+    Offset offset = Offset(0.0, y);
+    tp.paint(canvas, offset);
+  }
+
+  List<Offset> getCoordinates(Size size) {
+    List<Offset> coordinates = [];
+
+    double maxData = data.reduce(max);
+
+    double width = size.width - leftPadding;
+    double minBarWidth = width / data.length;
+
+    for (var index = 0; index < data.length; index++) {
+      double left = minBarWidth * index + leftPadding;
+
+      double normalized = data[index] / maxData;
+      double height = size.height - bottomPadding;
+      double top = height - normalized * height;
+
+      Offset offset = Offset(left, top);
+      coordinates.add(offset);
+    }
+
+    return coordinates;
+  }
+
+  @override
+  bool shouldRepaint(_BarChartPainter old) {
+    return old.data != data;
+  }
+}
---
+++ lib/chart/line/line_widget.dart
@@ -0,0 +1,145 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class LineChart extends StatelessWidget {
+  LineChart({
+    @required this.data,
+    this.size = const Size(150, 150),
+  }) : assert(data != null);
+
+  final List data;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return new LimitedBox(
+      child: new CustomPaint(
+        size: size,
+        painter: new _LineChartPainter(
+           points: data,
+           pointSize: 5.0, 
+           pointColor: Colors.pinkAccent, 
+           lineColor: Colors.pinkAccent, 
+           lineWidth: 2.0),
+      ), // CustomPaint
+    ); // LimitedBox
+  }
+}
+
+class _LineChartPainter extends CustomPainter {
+  List<double> points;
+  double lineWidth;
+  double pointSize;
+  Color lineColor;
+  Color pointColor;
+  int maxValueIndex;
+  int minValueIndex;
+  double fontSize = 18.0;
+
+  _LineChartPainter({this.points, this.pointSize, this.lineWidth, this.lineColor, this.pointColor});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    List<Offset> offsets = getCoordinates(points, size);
+
+    drawText(canvas, offsets);
+
+    drawLines(canvas, size,  offsets);
+    drawPoints(canvas, size, offsets);
+  }
+
+  void drawLines(Canvas canvas, Size size, List<Offset> offsets) {
+    Paint paint = Paint()
+      ..color = lineColor
+      ..strokeWidth = lineWidth
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
+
+    Path path = Path();
+
+    double dx = offsets[0].dx;
+    double dy = offsets[0].dy;
+
+    path.moveTo(dx, dy);
+    offsets.map((offset) => path.lineTo(offset.dx , offset.dy)).toList();
+
+    canvas.drawPath(path, paint);
+  }
+
+  void drawPoints(Canvas canvas, Size size, List<Offset> offsets) {
+    Paint paint = Paint()
+        ..color = pointColor
+        ..strokeCap = StrokeCap.round
+        ..strokeWidth = pointSize;
+
+    canvas.drawPoints(PointMode.points, offsets, paint);
+  }
+
+  List<Offset> getCoordinates(List<double> points, Size size) {
+    List<Offset> coordinates = [];
+
+    double spacing = size.width / (points.length - 1);
+    double maxY = points.reduce(max);
+    double minY = points.reduce(min);
+
+    double bottomPadding = fontSize * 2;
+    double topPadding = bottomPadding * 2;
+    double h = size.height - topPadding;
+
+    for (int index = 0; index < points.length; index++) {
+      double x = spacing * index;
+      double normalizedY = points[index] / maxY; // Normalize
+      double y = getYPos(h, bottomPadding, normalizedY); 
+
+      Offset coord = Offset(x, y);
+      coordinates.add(coord);
+
+      findMaxIndex(points, index, maxY, minY);
+      findMinIndex(points, index, maxY, minY);
+    }
+
+    return coordinates;
+  }
+
+  double getYPos(double h, double bottomPadding, double normalizedY) => (h + bottomPadding) - (normalizedY * h);
+
+  void findMaxIndex(List<double> points, int index, double maxY, double minY) {
+    if (maxY == points[index]) {
+      maxValueIndex = index;
+    }
+  }
+
+  void findMinIndex(List<double> points, int index, double maxY,double minY) {
+    if (minY == points[index]) {
+      minValueIndex = index;
+    }
+  }
+
+  void drawText(Canvas canvas, List<Offset> offsets) {
+    String maxValue = "${points.reduce(max)}";
+    String minValue = "${points.reduce(min)}";
+
+    drawTextValue(canvas, minValue, offsets[minValueIndex], false);
+    drawTextValue(canvas, maxValue, offsets[maxValueIndex], true);
+  }
+
+  void drawTextValue(Canvas canvas, String text, Offset pos, bool textUpward) {
+    TextSpan maxSpan = TextSpan(style: TextStyle(fontSize: fontSize, color: Colors.black, fontWeight: FontWeight.bold), text: text);
+    TextPainter tp = TextPainter(text: maxSpan, textDirection: TextDirection.ltr);
+    tp.layout();
+
+    double y = textUpward ? -tp.height * 1.5  : tp.height * 0.5;
+    double dx = pos.dx - tp.width / 2;
+    double dy = pos.dy + y;
+
+    Offset offset = Offset(dx, dy);
+
+    tp.paint(canvas, offset);
+  }
+
+  @override
+  bool shouldRepaint(_LineChartPainter oldDelegate) {
+    return oldDelegate.points != points;
+  }
+}
